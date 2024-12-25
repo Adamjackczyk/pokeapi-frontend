964aa937e1cedd0602324bf3d74c87c6ec60cbb5
Jack Adamczyk <prepinavest@gmail.com>
2024-12-25 05:58:46 -0500

    Add Footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer">
-      <p className="footer__text">
-        &copy; {new Date().getFullYear()} PokeAPI Frontend. All rights reserved.
-      </p>
+      <div className="footer__content">
+        <p className="footer__text">
+          © {currentYear} PokéAPI Frontend. Data provided by PokéAPI.
+        </p>
+        <div className="footer__links">
+          <Link to="/" className="footer__link">
+            Home
+          </Link>
+          <Link to="/data" className="footer__link">
+            Pokémon Data
+          </Link>
+          <a
+            href="https://pokeapi.co"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer__link"
+          >
+            PokéAPI
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
